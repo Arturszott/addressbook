06c7e9463eef670320adf2bfbfcac0876bef7efe
Artur Szott <artur.szott@gmail.com>
2020-10-04 18:14:33 +0200

    Small adjustments

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders Home link', () => {
+test('renders Home and Settings link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Home/i);
+  const secondLinkElement = getByText(/Settings/i);
 
   expect(linkElement).toBeInTheDocument();
+  expect(secondLinkElement).toBeInTheDocument();
 });
